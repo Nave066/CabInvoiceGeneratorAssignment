a417218a065ace6685d667194e8b34b6854f1220
Naveen V <jayamnaveen68@gmail.com>
2022-02-17 13:35:00 +0530

    Add project files.

---
+++ CabInvoiceGeneratorAssignment/Program.cs
@@ -0,0 +1,17 @@
+﻿using CabInvoiceGeneratorAssignment;
+
+
+Console.WriteLine("Welcome to CabInvoice Generator");
+
+#region Normal Customers
+InvoiceGenerator invoiceGenerator = new InvoiceGenerator(RideType.NORMAL);
+double fare = invoiceGenerator.CalculateFare(2.0, 5);
+Console.WriteLine($"Fare for Normal customer : {fare}");
+Console.ReadLine();
+#endregion
+#region Premium Customer
+InvoiceGenerator invoiceGeneratorforPremium = new InvoiceGenerator(RideType.PREMIUM);
+double fareforpremium = invoiceGeneratorforPremium.CalculateFare(2.0, 5);
+Console.WriteLine($"Fare for Premium Customer : {fareforpremium}");
+Console.ReadLine();
+#endregion
